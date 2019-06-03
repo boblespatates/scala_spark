776439edf220eb7d7fb4cda1408be39c6ff964f2
boblespatates <noelzablocki@yahoo.fr>
2019-06-03 10:53:06 +0200

    Update Spark_verif.scala

--- Spark_verif.scala
+++ Spark_verif.scala
@@ -29,3 +29,79 @@ object Spark_verif {
 
   }
 }
+
+
+/* Uber use case
+
+
+ // SparkSession definition
+  val spark = SparkSession
+    .builder
+    .appName("Simple Application")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  // Define the Schema of inout files
+  val schema = StructType(Array(
+    StructField("dt", TimestampType, true),
+    StructField("lat", DoubleType, true),
+    StructField("lon", DoubleType, true),
+    StructField("base",  StringType, true)
+  ))
+
+  // Path of the input file
+  //val input_filepath = "C:\\Users\\nzablocki\\Documents\\uber_use_case\\input\\uber-raw-data-apr14.csv"
+  val input_filepath = "hdfs:///tmp/data/uber-raw-data-apr14.csv"
+
+  // Load Dataset
+  val df = spark.read
+    .format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(input_filepath)
+
+  println("df")
+  df.show()
+
+  // Time is not well infered, so there is a need to change the schema
+  val df_clean = df.select(df("Lat"),
+    df("Lon"),
+    df("Base"),
+    unix_timestamp(df("Date/Time"), "MM/dd/yyyy HH:mm:ss").cast(TimestampType).as("timestamp")
+  )
+
+  // Display DataFrame info
+  df_clean.cache()
+  df_clean.show()
+  df_clean.printSchema()
+
+  // Put all the features of a row in a vector
+  val featureCols = Array("Lat", "Lon")
+  val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
+  val df2 = assembler.transform(df_clean)
+
+  df2.show()
+
+  // Clustering Part
+  val Array(trainingData, testData) = df2.randomSplit(Array(0.7,0.3),5043)
+  val kmeans = new KMeans().setK(20).setFeaturesCol("features").setMaxIter(5)
+  val model = kmeans.fit(trainingData)
+
+  // Display clusters coordinates
+  println("Final Centers: ")
+  model.clusterCenters.foreach(println)
+
+  // Evaluate the model
+  val categories = model.transform(testData)
+  categories.show()
+
+  // Which hour of the day and which cluster has the hiest amount of data
+  categories.select(hour(categories("timestamp")).alias("hour"), categories("prediction"))
+    .groupBy("hour", "prediction")
+    .agg(count("prediction").alias("count"))
+    .orderBy(desc("count"))
+    .show()
+
+  categories.groupBy("prediction").count().show()
+
+*/
